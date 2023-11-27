4176b9cfae9408b78c1fc0bd3308d4f4b4600d0a
alihaghayegh <haghayeghali95@gmail.com>
2023-11-27 20:15:08 +0330

    base model for items added to core/models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField()
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name_plural = "item"
