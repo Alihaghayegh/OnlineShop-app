4ed4e1c2b9e75711eae68249f4f88ae9ac6e1fd9
alihaghayegh <haghayeghali95@gmail.com>
2023-12-14 21:43:55 +0330

    after some days and trying some futures now top bar is responsive

--- frontend/src/components/productCard/ProductCard.jsx
+++ frontend/src/components/productCard/ProductCard.jsx
@@ -1,29 +1,22 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+import test from "../../assets/image/toy-car.jpg";
+
+import "./ProductCard.css";
 
 export default function ProductCard() {
-  const [items, setItems] = useState([]);
-
-  const fetchData = () => {
-    axios
-      .get("http://localhost:8000/api/items?format=json")
-      .then((res) => {
-        setItems(res.data);
-        console.log(res.data)
-      })
-      .catch((error) => {
-        console.log("Error fetching data: ", error);
-      });
-  };
-
   return (
-    <div>
-      <button onClick={fetchData}>Fetch Data</button>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item.name}</li>
-        ))}
-      </ul>
-    </div>
+      <div className="card-container">
+        <img src={test} alt="pic" className="card-image" />
+        <div className="titlePrice">
+          <h2 className="title">عنوان تست</h2>
+          <h2 className="price">10000$</h2>
+        </div>
+        {/* <p className="description">
+          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+          ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
+          کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+        </p> */}
+      </div>
   );
 }
--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,14 +1,13 @@
 import Topbar from "./Topbar/Topbar";
-import ProductCard from "../components/productCard/ProductCard"
+// import ProductCard from "../components/productCard/ProductCard";
 
-import './Layout.css'
+import "./Layout.css";
 
 export default function Layout(props) {
   return (
     <div>
       <Topbar />
       <div className="content-container">
-        <ProductCard />
         {props.children}
       </div>
     </div>
