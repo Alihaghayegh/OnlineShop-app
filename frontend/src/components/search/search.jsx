1b6561487eb6bbab4f7a134805492c4c0fb041fc
alihaghayegh <haghayeghali95@gmail.com>
2023-12-30 18:09:42 +0330

    sizing in search box refactored

--- frontend/src/components/search/search.jsx
+++ frontend/src/components/search/search.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
@@ -5,13 +6,15 @@ import Image from "react-bootstrap/Image";
 import search from "../../assets/image/search.png";
 
 export default function Search() {
+  const location = useLocation();
+
   return (
-    <div>
+    <div className={location.pathname === "/" ? "w-75" : ""}>
       <Form className="d-flex">
         <Form.Control
           type="search"
           placeholder="جست و جو"
-          className="mx-2"
+          className="mx-2 rounded-4"
           aria-label="Search"
         />
         <Button variant="outline-light">
