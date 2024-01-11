d65e464cbb189c10185140d5566940dcdd2b569b
alihaghayegh <haghayeghali95@gmail.com>
2024-01-11 19:41:48 +0330

    drf-spectacular[sidecar] added to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'drf_spectacular',
+    'drf_spectacular_sidecar',
     'category',
     'core',
     'search',
@@ -174,6 +175,9 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'project description',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_DIST': 'SIDECAR',
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR',
 }
 
 # Media shit
